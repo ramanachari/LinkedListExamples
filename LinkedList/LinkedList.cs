8b5836650bedd3780d0b1b659b2a3f76b907ec35
vrallabandi <vrallabandi@dealersocket.com>
2020-09-21 23:00:08 +0530

    Linked list insert and reverse

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node NextNode { get; set; }
+        public Node(int data)
+        {
+            this.Data = data;
+            NextNode = null;
+        }
+        
+    }
+
+    class LinkedList
+    {
+        Node list = null;
+
+        public void InsertFront(int data)
+        {
+            Node newNode = new Node(data);
+            if (list == null)
+            {
+                list = newNode;
+                return;
+            }
+
+            newNode.NextNode = list;
+            list = newNode;
+        }
+
+        public void InsertRare(int data)
+        {
+            Node newNode = new Node(data);
+            if (list == null)
+            {
+                list = newNode;
+                return;
+            }
+            Node temp = list;
+            while (temp.NextNode != null)
+            {
+                temp = temp.NextNode;
+            }
+            temp.NextNode = newNode;
+        }
+
+        public void InsertAtPosition(int data,int position)
+        {
+            int currentPosition = 1;
+            Node temp = list;
+
+            while (currentPosition < position)
+            {
+                temp = temp.NextNode;
+                currentPosition++;
+            }
+            Node newNode = new Node(data);
+            newNode.NextNode = temp.NextNode;
+            temp.NextNode = newNode;
+        }
+
+        public void ReverseList()
+        {
+            Node current = list,prev=null,next;
+            list = null;
+            while (current != null)
+            {
+                next = current.NextNode;
+                current.NextNode = prev;
+                prev = current;
+                current = next;
+            }
+            list = prev;
+        }
+
+        public void PrintList()
+        {
+            Console.WriteLine();
+            Node temp = list;
+            while (temp != null)
+            {
+                Console.Write(temp.Data+" ");
+                temp = temp.NextNode;
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace LinkedList
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            char ch;
+            int data;
+            LinkedList linkedList = new LinkedList();
+            do
+            {
+
+                Console.WriteLine("1. Insert First");
+                Console.WriteLine("2. Insert Last");
+                Console.WriteLine("3. Insert At spec position");
+                Console.WriteLine("4. Reverse List");
+                Console.WriteLine("<<< Enter Choice >>>");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter node data");
+                        data= Convert.ToInt32(Console.ReadLine());
+                        linkedList.InsertFront(data);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter node data");
+                        data=Convert.ToInt32(Console.ReadLine());
+                        linkedList.InsertRare(data);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter node data");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter position");
+                        int pos= Convert.ToInt32(Console.ReadLine());
+                        linkedList.InsertAtPosition(data,pos);
+                        break;
+                    case 4:
+                        linkedList.ReverseList();
+                        break;
+                }
+                Console.WriteLine("Linked List Elements are: ");
+                linkedList.PrintList();
+
+                Console.WriteLine("Press Y to continue");
+                ch =Console.ReadLine()[0];
+
+            } while (ch != 'Y' || ch != 'y');
+        }
+    }
+}
